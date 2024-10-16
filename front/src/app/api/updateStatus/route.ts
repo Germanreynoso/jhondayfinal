03f9680159972a23cb59814849c70316feeff14b
germanreynoso <germanreynoso94@gmail.com>
2024-10-16 15:02:07 -0300

    cambio updatestatus

--- front/src/app/api/updateStatus/route.ts
+++ front/src/app/api/updateStatus/route.ts
@@ -1,23 +1,17 @@
-// pages/api/updateAppointment.ts
-import { NextApiRequest, NextApiResponse } from 'next';
-import prisma from '@/app/lib/prisma';
+import { NextResponse } from 'next/server';
+import prisma from '@/app/lib/prisma'; // Asegúrate de que esta ruta sea correcta
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
-    const { appointmentId, status } = req.body;
+export async function POST(request: Request) {
+  const { appointmentId, status } = await request.json(); // Obtener los datos del cuerpo de la solicitud
 
-    try {
-      const updatedAppointment = await prisma.appointment.update({
-        where: { id: appointmentId },
-        data: { status },
-      });
-      res.status(200).json(updatedAppointment);
-    } catch (error) {
-      console.error('Error actualizando la cita:', error);
-      res.status(500).json({ error: 'Error actualizando la cita' });
-    }
-  } else {
-    res.setHeader('Allow', ['POST']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+  try {
+    const updatedAppointment = await prisma.appointment.update({
+      where: { id: appointmentId },
+      data: { status },
+    });
+    return NextResponse.json(updatedAppointment); // Devolver la cita actualizada como respuesta
+  } catch (error) {
+    console.error('Error actualizando la cita:', error);
+    return NextResponse.json({ error: 'Error actualizando la cita' }, { status: 500 });
   }
 }
