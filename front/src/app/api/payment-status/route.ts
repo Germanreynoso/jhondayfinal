940268ffee94bf94e1013394315ef3adcc6a293d
germanreynoso <germanreynoso94@gmail.com>
2024-10-16 14:35:54 -0300

    cambio en payment-status

--- front/src/app/api/payment-status/route.ts
+++ front/src/app/api/payment-status/route.ts
@@ -1,27 +1,29 @@
-// pages/api/payment-status.ts
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextResponse } from 'next/server';
 import Stripe from 'stripe';
 
-const stripe = new Stripe("sk_test_51Q7otE00ffkRUeeSNKqMZLAVvxk5Eb0hwkkw7cVVYWiWPIPPTSe2fu4p3074vbNxPALEERydW5Vxw6VrKLVgpoq100Y1WEh078", {
+const stripe = new Stripe("sk_test_51Q7otE00ffkRUeeSNKqMZLAVvxk5Eb0hwkkw7cVVYWiWPIPPTSe2fu4p3074vbNxPALEERydW5Vxw6VrKLVgpoq100Y1WEh078", {});
 
-});
+export async function GET(request: Request) {
+  // Obtiene el session_id de la URL
+  const { searchParams } = new URL(request.url);
+  const sessionId = searchParams.get('session_id');
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { session_id } = req.query;
+  if (!sessionId) {
+    return NextResponse.json({ error: 'session_id is required' }, { status: 400 });
+  }
 
   try {
-    const session = await stripe.checkout.sessions.retrieve(session_id as string);
+    const session = await stripe.checkout.sessions.retrieve(sessionId);
 
     if (session.payment_status === 'paid') {
-      // Suponiendo que el appointmentId está en los metadatos de la sesión
       const appointmentId = session.metadata?.appointmentId;
 
-      res.status(200).json({ status: 'paid', appointmentId });
+      return NextResponse.json({ status: 'paid', appointmentId });
     } else {
-      res.status(400).json({ status: 'unpaid' });
+      return NextResponse.json({ status: 'unpaid' }, { status: 400 });
     }
   } catch (error) {
     console.error('Error retrieving Stripe session:', error);
-    res.status(500).json({ error: 'Error retrieving payment status' });
+    return NextResponse.json({ error: 'Error retrieving payment status' }, { status: 500 });
   }
 }
